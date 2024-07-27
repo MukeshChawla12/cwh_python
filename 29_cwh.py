ba01b35fbfd029951f96fd50b83f25b71ff6eb09
unknown <chawlamukesh109@gmail.com>
2024-07-27 13:10:44 +0530

    Recursion

--- 29_cwh.py
+++ 29_cwh.py
@@ -1,5 +1,5 @@
-##Recursion Program 
-#printing a table of a given number using recursive function
+#Recursion Program 
+# printing a table of a given number using recursive function
 def tab_rec_fun(n, i=1):
     if(i>10):
         return
@@ -8,6 +8,15 @@ def tab_rec_fun(n, i=1):
 num = int(input("Enter a number : "))
 print("\nTable of", num, "is:")
 tab_rec_fun(num)
+##fibonacci series usinf recursive function
+def fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1) + fibo(n-2)
+terms=int(input("Enter the no. of terms :"))
+for i in range(terms):
+    print(fibo(i),end=" ")
 
    
 
